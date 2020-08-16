2925193357b90c8b0a1b9e6c3f2672924847a406
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-15 20:52:18 -0600

    Add index and create to Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :expenses
-  resources :users
+  resources :articles, only: [:index]
+  resources :expenses, only: [:index, :create]
+  resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
