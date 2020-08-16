6c9274bb7d5ae37c911c481a7888237acfefcee9
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-15 20:55:06 -0600

    Provide 9 Article seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+Article.create(title: "Strategic Philanthropy For Young Adults", url: "https://www.forbes.com/sites/halseyschreier/2020/08/13/personal-finance-for-young-adults-strategic-philanthropy/#27d727816e28", image: "https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
+
+Article.create(title: "Cut Your Spending With These Finance Tips", url: "https://www.msn.com/en-us/money/other/cut-your-spending-with-these-personal-finance-tips/ss-BB17SICF?li=BBnb7Kz", image: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB17SKFO.img?h=416&w=799&m=6&q=60&u=t&o=f&l=f&x=111&y=157")
+
+Article.create(title: "5 Rules To Improve Your Financial Health", url: "https://www.investopedia.com/articles/personal-finance/111813/five-rules-improve-your-financial-health.asp", image: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB17STyL.img?h=416&w=799&m=6&q=60&u=t&o=f&l=f")
+
+Article.create(title: "7 Ways To Build Your Credit Fast", url: "https://www.nerdwallet.com/article/finance/raise-credit-score-fast", image: "https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")
+
+Article.create(title: "25 Ways To Make Money Online And Offline", url: "https://www.nerdwallet.com/blog/finance/how-to-make-money-at-home-and-online/", image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80")
+
+Article.create(title: "5 Keys To Managing Your Finances", url: "https://www.thebalance.com/manage-your-personal-finances-2385812", image: "https://images.unsplash.com/photo-1584473457493-17c4c24290c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
+
+Article.create(title: "Should You Buy New Or Used Items?", url: "https://www.thebalance.com/should-you-buy-new-or-used-4054264", image: "https://images.unsplash.com/photo-1531303435785-3853ba035cda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
+
+Article.create(title: "Creating A Monthly Budget For Your Personal Finances", url: "https://www.thebalance.com/creating-a-monthly-budget-316118", image: "https://images.unsplash.com/photo-1573164574308-edcb95e8b261?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80")
+
+Article.create(title: "10 Things To Know About An HSA", url: "https://www.thebalance.com/health-savings-account-hsa-1289562", image: "https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
+
