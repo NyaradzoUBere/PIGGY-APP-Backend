b066ed655defc1da6f0594fae89c58bca8b2e603
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-15 21:22:54 -0600

    Add authenticate to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    def authenticate
+        begin
+            header = request.headers["Authorization"]
+            token = header.split(" ")[1]
+            secret = Rails.application.secret_key_base
+            payload = JWT.decode(token, secret)[0]
+        rescue
+            render json: {error: "Must be logged in to access"}
+        end
+    end
 end
