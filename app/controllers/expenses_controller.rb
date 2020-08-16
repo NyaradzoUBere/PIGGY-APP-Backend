a687f6bf6c605ff583c3c9b719426ad686863029
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-15 20:58:46 -0600

    Add index and create to Expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,15 @@
 class ExpensesController < ApplicationController
+    def index
+        @expenses = Expense.all
+        render json: @expenses
+    end
+
+    def create
+        @expense = Expense.create(
+            user_id: params[:user_id],
+            item: params[:item],
+            amount: params[:amount],
+        )
+        render json: @expense
+    end
 end
